4d4eb872b68e20e2e1ba9daade944cd94994605b
JeremyTobNYU <jerem.tob@gmail.com>
2026-02-13 17:23:53 -0500

    Added the post guess logic

--- app.rb
+++ app.rb
@@ -40,7 +40,12 @@ class WordGuesserApp < Sinatra::Base
   # If a guess is invalid, set flash[:message] to "Invalid guess."
   post '/guess' do
     params[:guess].to_s[0]
-    ### YOUR CODE HERE ###
+    letter = params[:guess].to_s[0] || ""
+
+    game = session[:game]
+
+    game.guess(letter)
+
     redirect '/show'
   end
 
