1a7e5178bf6fbcf72fcca589c9b53098ee6730fc
JeremyTobNYU <jerem.tob@gmail.com>
2026-02-13 16:30:48 -0500

    Finish part 2 wordguesser app

--- lib/wordguesser_game.rb
+++ lib/wordguesser_game.rb
@@ -4,9 +4,15 @@ class WordGuesserGame
 
   # Get a word from remote "random word" service
 
+  attr_accessor :word
+  attr_reader :guesses, :wrong_guesses
+
   def initialize(word)
     @word = word
+    @guesses = ""
+    @wrong_guesses = ""
   end
+
 
   # You can test it by installing irb via $ gem install irb
   # and then running $ irb -I. -r app.rb
@@ -15,9 +21,58 @@ class WordGuesserGame
   def self.get_random_word
     require 'uri'
     require 'net/http'
-    uri = URI('https://esaas-randomword-27a759b6224d.herokuapp.com/RandomWord') 
-    Net::HTTP.start(uri.host, uri.port, use_ssl: true) do |http| 
+    uri = URI('https://esaas-randomword-27a759b6224d.herokuapp.com/RandomWord')
+    Net::HTTP.start(uri.host, uri.port, use_ssl: true) do |http|
       return http.post(uri, "").body
     end
   end
+
+  def guess(letter)
+
+    if letter.nil? || letter.length != 1 || !(letter =~ /[a-zA-Z]/) #All the possible error inputs
+      raise ArgumentError
+    end
+    
+    letter = letter.downcase
+
+    # already guessed (either correct or wrong)
+    if @guesses.include?(letter) || @wrong_guesses.include?(letter) 
+      return false
+    end
+
+    if @word.include?(letter)
+      @guesses += letter
+      return true
+    else
+      @wrong_guesses += letter
+      return false
+    end
+  end
+
+  def word_with_guesses
+    result = ""
+
+    @word.each_char do |c|
+      if @guesses.include?(c)
+        result += c
+      else
+        result += "-"
+      end
+    end
+
+    return result
+  end
+
+  def check_win_or_lose
+    if word_with_guesses == @word
+      return :win
+    end
+
+    if @wrong_guesses.length >= 7
+      return :lose
+    end
+
+    return :play
+  end
+
 end
