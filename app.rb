9bf3ad6c88697d9ec2ca1727a2eebd31483a7157
JeremyTobNYU <jerem.tob@gmail.com>
2026-02-13 17:30:09 -0500

    Connected the /win feature

--- app.rb
+++ app.rb
@@ -55,15 +55,27 @@ class WordGuesserApp < Sinatra::Base
   # Notice that the show.erb template expects to use the instance variables
   # wrong_guesses and word_with_guesses from @game.
   get '/show' do
-    ### YOUR CODE HERE ###
-    erb :show # You may change/remove this line
+    @game = session[:game]
+
+    if @game.nil?
+      redirect '/new'
+    else
+      erb :show
+    end
   end
 
   get '/win' do
-    ### YOUR CODE HERE ###
-    erb :win # You may change/remove this line
-  end
+    @game = session[:game]
 
+    if @game.nil?
+      redirect '/new'
+    elsif @game.check_win_or_lose != :win
+      redirect '/show'
+    else
+      erb :win
+    end
+  end
+  
   get '/lose' do
     ### YOUR CODE HERE ###
     erb :lose # You may change/remove this line
